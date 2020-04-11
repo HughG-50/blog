3340b5bfcdbc7134ceb538a6f5b07b2d2f8c1313
hughgreethead <hugh.greethead@gmail.com>
2020-04-11 19:17:10 +1000

    Updated article view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Declares it as a standard REST resource - i.e. sets routes of GET, POST, PUT PATCH DELETE to REST convention
   resources :articles
 
   root 'welcome#index'
